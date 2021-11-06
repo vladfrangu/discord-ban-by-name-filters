ea07dac2749fb4bce5a085f552207f6023c363a8
Vlad Frangu <kingdgrizzle@gmail.com>
2021-11-07 00:23:17 +0200

    chore: safety is number 1 priority

--- src/commands/Filters/addfilter.ts
+++ src/commands/Filters/addfilter.ts
@@ -4,6 +4,13 @@ import type { Message } from 'discord.js';
 import { createInfoEmbed } from '../../lib/utils/createInfoEmbed';
 import { addFilter } from '../../lib/utils/filters';
 
+const onlyUsersWhoCanAddFullRegexes = [
+	// Vladdy#0002
+	'139836912335716352',
+	// Trophias#0001
+	'85185381837836288',
+];
+
 @ApplyOptions<CommandOptions>({
 	description: 'Adds a filter',
 	preconditions: ['AdminOnly'],
@@ -12,7 +19,11 @@ export default class extends Command {
 	public async messageRun(message: Message, args: Args) {
 		const filter = await args.rest('string');
 
-		const result = await addFilter(filter);
+		const result = await addFilter(
+			onlyUsersWhoCanAddFullRegexes.includes(message.author.id)
+				? filter
+				: filter.replace(/[\\^$*+?.()|[\]{}]/g, '\\$&'),
+		);
 
 		return message.channel.send({
 			embeds: [
